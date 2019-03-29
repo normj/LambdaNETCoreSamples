3a7837b36ebd5578ab42c14c4540f4d5a1333782
Norm Johanson <normj@amazon.com>
2019-03-28 23:17:21 -0700

    Add CustomRuntimeAspNetCore sample

---
+++ CustomRuntimeAspNetCore/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.RuntimeSupport;
+using Amazon.Lambda.Serialization.Json;
+
+namespace CustomRuntimeAspNetCore
+{
+    /// <summary>
+    /// The Main function can be used to run the ASP.NET Core application locally using the Kestrel webserver.
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if(string.IsNullOrEmpty(Environment.GetEnvironmentVariable("AWS_LAMBDA_FUNCTION_NAME")))
+            {
+                BuildWebHost(args).Run();
+            }
+            else
+            {
+                var lambdaEntry = new LambdaEntryPoint();
+                var functionHandler = (Func<APIGatewayProxyRequest, ILambdaContext, Task<APIGatewayProxyResponse>>)(lambdaEntry.FunctionHandlerAsync);
+                using(var handlerWrapper = HandlerWrapper.GetHandlerWrapper(functionHandler, new JsonSerializer()))
+                using(var bootstrap = new LambdaBootstrap(handlerWrapper))
+                {
+                    bootstrap.RunAsync().Wait();
+                }
+            }
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+## Sample .NET Core Lambda Functions
+
+| Name | Description|
+|------| -----------|
+| [CustomRuntimeAspNetCore](./CustomRuntimeAspNetCore/README.md) | An ASP.NET Core 2.2 project configured to use Amazon.Lambda.RuntimeSupport and deployed using Lambda's custom runtime feature. |
