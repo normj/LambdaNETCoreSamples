e2217c087fd98fb84cb046b8139bd3590d3ade8f
bschaatsbergen <git@bschaatsbergen.com>
2021-10-03 22:11:37 +0200

    Don't use implicit type for delegates

--- CustomRuntimeListBucketsNET6/Function.cs
+++ CustomRuntimeListBucketsNET6/Function.cs
@@ -1,18 +1,17 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.RuntimeSupport;
 using Amazon.Lambda.Serialization.SystemTextJson;
 
 using Amazon.S3;
+using Amazon.S3.Model;
 
 using var s3Client = new AmazonS3Client();
 
-// Lambda function that returns the list of S3 buckets.
-var listS3BucketsLambdaFunction = async (ILambdaContext context) =>
-{
-    return (await s3Client.ListBucketsAsync()).Buckets;
-};
+async Task<List<S3Bucket>> ListBucketsDelegate(ILambdaContext context) => (await s3Client.ListBucketsAsync()).Buckets;
 
 // Startup Lambda .NET runtime
-using var handlerWrapper = HandlerWrapper.GetHandlerWrapper(listS3BucketsLambdaFunction, new DefaultLambdaJsonSerializer());
+using var handlerWrapper = HandlerWrapper.GetHandlerWrapper(ListBucketsDelegate, new DefaultLambdaJsonSerializer());
 using var bootstrap = new LambdaBootstrap(handlerWrapper);
 await bootstrap.RunAsync();
